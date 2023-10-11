b809159abf76da988ce2d5cd5562b0ee60bf6a09
Joseph <josephmario_santiago@yahoo.com>
2023-10-11 19:56:38 +0700

    api view setup

--- APIProject/settings.py
+++ APIProject/settings.py
@@ -53,13 +53,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'APIProject.urls'
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
+# REST_FRAMEWORK = {
+#     # Use Django's standard `django.contrib.auth` permissions,
+#     # or allow read-only access for unauthenticated users.
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#     ]
+# }
 
 TEMPLATES = [
     {
--- api/views.py
+++ api/views.py
@@ -4,44 +4,44 @@ from django.shortcuts import render
 from .models import Person
 from .serializers import PersonSerializer
 from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
-@csrf_exempt
+
+@api_view(['GET', 'POST'])
 def person_list(request):
 
     if request.method == 'GET':
         person = Person.objects.all()
         serializer = PersonSerializer(person, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = PersonSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201) 
-        return JsonResponse(serializer.errors, status=400) 
+            return Response(serializer.data, status=201) 
+        return Response(serializer.errors, status=400) 
 
-
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def person_details(request, pk):
     try:
         person = Person.objects.get(pk=pk)
     except Person.DoesNotExist:
-        return HttpResponse(statue=404)
+        return HttpResponse(statue=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = PersonSerializer(person)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PersonSerializer(person, data=data)
+        serializer = PersonSerializer(person, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data) 
-        return JsonResponse(serializer.errors, status=400) 
+            return Response(serializer.data) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
     
     elif request.method == 'DELETE':
         person.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
